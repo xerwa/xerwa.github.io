15af262fec7caea6d72b267d35ef415327f19f7a
xerwa <49260362+xerwa@users.noreply.github.com>
2023-09-25 11:23:21 +0800

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.write("随便写点什么...")
+
+st.title("这是一个title")
+
+st.header("这是一个header")
+
+st.subheader("这是一个子header")
+
+st.caption("这是一个小写字体的标题")
+
+st.text("这是一段正常的文本内容")
+
+st.balloons()  # 气球特效
+
+st.snow()   # 雪花特效
